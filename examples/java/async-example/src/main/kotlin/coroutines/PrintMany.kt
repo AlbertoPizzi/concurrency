fd1d1e268fd85a89a5f9c428644f05db8a4d4599
Emilio L. Gabeiras <emiliolg@mac.com>
2025-06-03 20:12:08 -0300

    Update slides

--- examples/java/async-example/src/main/kotlin/coroutines/PrintMany.kt
+++ examples/java/async-example/src/main/kotlin/coroutines/PrintMany.kt
@@ -5,10 +5,10 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    for (i in 0..50_000) { // launch a lot of coroutines
-        launch {
+    for (i in 0..50) { // launch a lot of coroutines
+      //  launch {
             println("Hello $i!")
             delay(5000L)
-        }
+        //}
     }
 }
